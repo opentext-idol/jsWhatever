f179de3239c95c4546da722e4f9d621930c2bee7
Alex Scown <alex.scown@hp.com>
2014-09-23 14:04:03 +0000

    [javascript-utils] (FOSS-45) Replace rgb2hsb.  [rev: matthew.gordon2]
    [git-p4: depot-paths = "//depot/products/api/frontend/javascript-utils/trunk/": change = 1176555]

--- src/main/exports/javascript-utils/js/colour-manager.js
+++ src/main/exports/javascript-utils/js/colour-manager.js
@@ -22,6 +22,49 @@ define([
         }
     };
 
+    var rgb2hsb = function(r, g, b) {
+         r = r / 255;
+         g = g / 255;
+         b = b / 255;
+
+        var max = Math.max(r, g, b);
+        var min = Math.min(r, g, b);
+
+        var c = (max - min);
+        var s;
+
+        if(max !== 0) {
+            s = c / max;
+        }
+        else {
+            s = 0;
+        }
+
+        var h;
+
+        if(c === 0) {
+            h = 0;
+        }
+        else if(max === r) {
+            h = (g - b) / c;
+        }
+        else if(max === g) {
+            h = 2 + (b - r) / c;
+        }
+        else {
+            h = 4 + (r - g) / c;
+        }
+
+        // +360 to avoid changes when compared to Raphael
+        h = ((h + 360) % 6) / 6;
+
+        return {
+            h: h,
+            s: s,
+            b: max
+        }
+    };
+
     // getColour maps user-defined keys to a (hopefully) unique colour.
 
     var predefinedColours = function() {
@@ -71,7 +114,7 @@ define([
 
         changeSaturation: function (hexIn, fraction) {
             var rgb = hex2Rgb(hexIn);
-            var hsb = Raphael.rgb2hsb(rgb.r, rgb.g, rgb.b);
+            var hsb = rgb2hsb(rgb.r, rgb.g, rgb.b);
             hsb.s = Math.min(1, hsb.s * fraction);
             return Raphael.hsb(hsb.h, hsb.s, hsb.b);
         }
