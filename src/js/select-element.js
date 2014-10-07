b739f33be235a5666f07b46ae10b2af86558526d
Alex Scown <alex.scown@hp.com>
2014-10-07 15:51:42 +0100

    Add comments for clarity. [rev: trivial]

--- src/js/select-element.js
+++ src/js/select-element.js
@@ -8,6 +8,7 @@ define([
             return false;
         }
 
+        // real browsers
         if (window.getSelection) {
             var sel = window.getSelection();
             sel.removeAllRanges();
@@ -15,7 +16,7 @@ define([
             range.selectNodeContents(el[0]);
             sel.addRange(range);
             return true;
-        } else if (document.selection) {
+        } else if (document.selection) { // IE < 9
             var textRange = document.body.createTextRange();
             textRange.moveToElementText(el[0]);
             textRange.select();
