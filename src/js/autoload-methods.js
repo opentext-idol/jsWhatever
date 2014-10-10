d9a6a00ce41de6a962db5ecfcfc58f0a87d6d617
Alex Scown <alex.scown@hp.com>
2014-10-10 10:28:54 +0100

    Fix whitespace, remove commented out code, replace useless function with $.noop.

--- src/js/autoload-methods.js
+++ src/js/autoload-methods.js
@@ -1,67 +1,62 @@
-define({
+define([
+    'jquery',
+    'underscore'
+],function($, _) {
 
-    autoload: true,
+    return {
+        autoload: true,
+        loaded: false,
 
-    loaded: false,
+        initialize : function(initialState, options) {
+            // We need our .loaded flag to be set true in a change listener since it's fired before the fetch()
+            // completion handler fires; prevents loss of the first change event if we call this.onLoad from within
+            // this.onLoad e.g. in #1018992. We still keep the fetch completion handler since it's not explicitly
+            // mentioned in docs whether the handler or change() events fire first () so it might change in future.
+            options = options || {};
 
-    initialize : function(initialState, options) {
-        // We need our .loaded flag to be set true in a change listener since it's fired before the fetch()
-        // completion handler fires; prevents loss of the first change event if we call this.onLoad from within
-        // this.onLoad e.g. in #1018992. We still keep the fetch completion handler since it's not explicitly
-        // mentioned in docs whether the handler or change() events fire first () so it might change in future.
-        options = options || {};
+            var onLoaded = _.bind(function(){
+                this.off(this.eventName, onLoaded);
+                this.loaded = true;
+            }, this);
 
-        var onLoaded = _.bind(function(){
-            this.off(this.eventName, onLoaded);
-            this.loaded = true;
-        }, this);
+            if (options.ports) {
+                this.ports = options.ports;
+            }
 
-        if (options.ports) {
-            this.ports = options.ports;
-        }
+            if (options.autoload !== undefined) {
+                this.autoload = options.autoload
+            }
 
-        if (options.autoload !== undefined) {
-            this.autoload = options.autoload
-        }
+            if (options.loaded !== undefined) {
+                this.loaded = options.loaded
+            }
 
-        if (options.loaded !== undefined) {
-            this.loaded = options.loaded
-        }
+            this.on(this.eventName, onLoaded);
 
-        this.on(this.eventName, onLoaded);
+            if (this.autoload) {
+                this.fetch({
+                    success: onLoaded
+                });
+            }
+        },
 
-        if (this.autoload) {
-            this.fetch({
-                success: onLoaded
-            });
-        }
+        ///Override to something like: "?action=GetStatus&ResponseFormat=json"
+        url: $.noop,
 
-        // Test code; doesn't fire the alert in the previous version on the first model fetch, would only fire if another change happened
-//        this.onLoad(_.bind(function(){
-//            this.onLoad(_.bind(function(){
-//                alert('i should be called');
-//            }, this));
-//        }, this));
-    },
+        onLoad: function(callback, ctx) {
+            if (ctx) {
+                callback = _.bind(callback, ctx);
+            }
 
-    ///Override to something like: "?action=GetStatus&ResponseFormat=json"
-    url: function() {
-        typeof console !== 'undefined' && console.log('You are being a very silly person.  Override the url property of your ACI Model!');
-    },
-
-    onLoad: function(callback, ctx) {
-		if (ctx) {
-			callback = _.bind(callback, ctx);
-		}
-
-        if (this.loaded) {
-            callback(this);
-        }
-        else {
-            this.on(this.eventName, function listener() {
-                this.off(this.eventName, listener);
+            if (this.loaded) {
                 callback(this);
-            });
+            }
+            else {
+                this.on(this.eventName, function listener() {
+                    this.off(this.eventName, listener);
+                    callback(this);
+                });
+            }
         }
     }
 });
