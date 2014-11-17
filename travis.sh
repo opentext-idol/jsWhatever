71e7f104d0a83b8506fc6392ab75e5c7c4890888
Alex Scown <alex.scown@hp.com>
2014-11-17 16:55:21 +0000

    Use correct task name...

--- travis.sh
+++ travis.sh
@@ -4,5 +4,5 @@ if [[ $TRAVIS_BRANCH == 'master' ]]
 then
   git config credential.helper "store --file=.git/credentials"
   echo "https://${GH_TOKEN}:@github.com" > .git/credentials
-  grunt push-doc-travs
+  grunt push-doc-travis
 fi
